49ee3465a21e2e1f792319602767603a9cfd7428
kfarnung <kfarnung@users.noreply.github.com>
2026-02-12 07:26:23 +0000

    deploy: 986b222d89e6a1dc9f816cd66cfac0d0b2e2cf01

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/puzztool/service-worker.js', { scope: '/puzztool/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/service-worker.js', { scope: '/' })})}
